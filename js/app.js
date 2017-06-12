71a6ce497dbbdfe4095d22702a428f90f9b33957
Gonzalez <Ericson.Gonzalez@yrbrands.com>
2017-06-12 15:49:34 -0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	var $container = $('.grid');
+	var options = {
+		itemSelector : '.grid-item',
+		columnwidth: '.grid-sizer',
+		isFitWidth: true,
+		percentPosition: true
+	};
+
+	masonryInit($container, options);
+
+	$('.main-nav .mobile').click(function() {
+		$('.main-nav .desktop').removeClass('slide-out');
+	});
+
+	$('.main-nav .desktop li.close').click(function() {
+		$('.main-nav .desktop').addClass('slide-out');
+	});
+	
+	$.fancybox.defaults.margin = [0,0];
+	$.fancybox.defaults.focus = false;
+	console.log($.fancybox.defaults);
+
+});
+
+function masonryInit(container, options) {
+	container.masonry(options);
+	container.imagesLoaded( function() {
+	}).progress(function (imgLoad, image) {
+		if (image.isLoaded) {
+			var $wrapper = $(image.img).parent();
+			$wrapper.removeClass('is-loading');
+		}
+	});
+}
+
